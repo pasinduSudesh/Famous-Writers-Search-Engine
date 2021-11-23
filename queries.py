3839f4bb0996e237427d67447295cc35222cdbda
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2021-11-23 21:19:24 +0530

    fixed queries

--- queries.py
+++ queries.py
@@ -1,291 +1,11 @@
 import json
 
-#best_filds
-def multi_match_agg_best(query, fields=['title','song_lyrics']):
-	print ("QUERY FIELDS")
-	print (fields)
-	q = {
-		"size": 986,
-		"explain": True,
-		"query": {
-			"multi_match": {
-				"query": query,
-				"fields": fields,
-				"operator": 'or',
-				"type": "best_fields"
-			}
-		},
-		"aggs": {
-			"Genre Filter": {
-				"terms": {
-					"field": "genre.keyword",
-					"size": 10
-				}
-			},
-			"Music Filter": {
-				"terms": {
-					"field": "music.keyword",
-					"size": 10
-				}
-			},
-			"Artist Filter": {
-				"terms": {
-					"field": "artist.keyword",
-					"size": 10
-				}
-			},
-			"Lyrics Filter": {
-				"terms": {
-					"field": "lyrics.keyword",
-					"size": 10
-				}
-			}
-		}
-	}
-
-	q = json.dumps(q)
-	return q
-
-
-def multi_match_agg_sort_best(query, sort_num, fields=['title','song_lyrics']):
-	print ('sort num is ',sort_num)
-	q = {
-		"size": sort_num,
-		"sort": [
-			{"views": {"order": "desc"}},
-		],
-		"query": {
-			"multi_match": {
-				"query": query,
-				"fields": fields,
-				"operator": 'or',
-				"type": "best_fields"
-			}
-		},
-		"aggs": {
-			"Genre Filter": {
-				"terms": {
-					"field": "genre.keyword",
-					"size": 10
-				}
-			},
-			"Music Filter": {
-				"terms": {
-					"field": "music.keyword",
-					"size": 10
-				}
-			},
-			"Artist Filter": {
-				"terms": {
-					"field": "artist.keyword",
-					"size": 10
-				}
-			},
-			"Lyrics Filter": {
-				"terms": {
-					"field": "lyrics.keyword",
-					"size": 10
-				}
-			}
-		}
-	}
-	q = json.dumps(q)
-	return q
 
 #cross_filds
-def multi_match_agg_cross(query, fields=['writer_name', 'writer_dob','writer_life_story']):
-	print ("QUERY FIELDS")
-	print (fields)
-	q = {
-		"size": 986,
-		"explain": True,
-		"query": {
-			"multi_match": {
-				"query": query,
-				"fields": fields,
-				"operator": 'or',
-				"type": "cross_fields"
-			}
-		},
-		"aggs": {
-			"Genre Filter": {
-				"terms": {
-					"field": "sinhala_genre.keyword",
-					"size": 10
-				}
-			},
-			"Music Filter": {
-				"terms": {
-					"field": "sinhala_music.keyword",
-					"size": 10
-				}
-			},
-			"Artist Filter": {
-				"terms": {
-					"field": "sinhala_artist.keyword",
-					"size": 10
-				}
-			},
-			"Lyrics Filter": {
-				"terms": {
-					"field": "sinhala_lyrics.keyword",
-					"size": 10
-				}
-			}
-		}
-	}
-
-	q = json.dumps(q)
-	return q
-
-
-def multi_match_agg_sort_cross(query, sort_num, fields=['title','song_lyrics']):
-	print ('sort num is ',sort_num)
-	q = {
-		"size": sort_num,
-		"sort": [
-			{"views": {"order": "desc"}},
-		],
-		"query": {
-			"multi_match": {
-				"query": query,
-				"fields": fields,
-				"operator": 'or',
-				"type": "cross_fields"
-			}
-		},
-		"aggs": {
-			"Genre Filter": {
-				"terms": {
-					"field": "sinhala_genre.keyword",
-					"size": 10
-				}
-			},
-			"Music Filter": {
-				"terms": {
-					"field": "sinhala_music.keyword",
-					"size": 10
-				}
-			},
-			"Artist Filter": {
-				"terms": {
-					"field": "sinhala_artist.keyword",
-					"size": 10
-				}
-			},
-			"Lyrics Filter": {
-				"terms": {
-					"field": "sinhala_lyrics.keyword",
-					"size": 10
-				}
-			}
-		}
-	}
-	q = json.dumps(q)
-	return q
-
-
-#phrase_filds
-def multi_match_agg_phrase(query, fields=['title','song_lyrics']):
-	print ("QUERY FIELDS")
-	print (fields)
-	q = {
-		"size": 986,
-		"explain": True,
-		"query": {
-			"multi_match": {
-				"query": query,
-				"fields": fields,
-				"operator": 'or',
-				"type": "cross_fields"
-			}
-		},
-		"aggs": {
-			"Genre Filter": {
-				"terms": {
-					"field": "genre.keyword",
-					"size": 10
-				}
-			},
-			"Music Filter": {
-				"terms": {
-					"field": "music.keyword",
-					"size": 10
-				}
-			},
-			"Artist Filter": {
-				"terms": {
-					"field": "artist.keyword",
-					"size": 10
-				}
-			},
-			"Lyrics Filter": {
-				"terms": {
-					"field": "lyrics.keyword",
-					"size": 10
-				}
-			}
-		}
-	}
-
-	q = json.dumps(q)
-	return q
-
-
-def multi_match_agg_sort_phrase(query, sort_num, fields=['title','song_lyrics']):
-	print ('sort num is ',sort_num)
-	q = {
-		"size": sort_num,
-		"sort": [
-			{"views": {"order": "desc"}},
-		],
-		"query": {
-			"multi_match": {
-				"query": query,
-				"fields": fields,
-				"operator": 'or',
-				"type": "cross_fields"
-			}
-		},
-		"aggs": {
-			"Genre Filter": {
-				"terms": {
-					"field": "genre.keyword",
-					"size": 10
-				}
-			},
-			"Music Filter": {
-				"terms": {
-					"field": "music.keyword",
-					"size": 10
-				}
-			},
-			"Artist Filter": {
-				"terms": {
-					"field": "artist.keyword",
-					"size": 10
-				}
-			},
-			"Lyrics Filter": {
-				"terms": {
-					"field": "lyrics.keyword",
-					"size": 10
-				}
-			}
-		}
-	}
-	q = json.dumps(q)
-	return q
-
-
-
-	# ====================================================================================
-
-	#cross_filds
 def multi_match_corss_fields(query, fields=['writer_name']):
 	print ("=== QUERY FIELDS ===")
 	print (fields)
-	q = {
+	es_query = {
 		"size": 104,
 		"explain": True,
 		"query": {
@@ -306,13 +26,14 @@ def multi_match_corss_fields(query, fields=['writer_name']):
 		}
 	}
 
-	q = json.dumps(q)
-	return q
+	es_query = json.dumps(es_query)
+	return es_query
 
+#phrase prefix
 def multi_match_phrase_prefix(query, fields=['writer_name']):
 	print ("=== QUERY FIELDS ===")
 	print (fields)
-	q = {
+	es_query = {
 		"size": 104,
 		"explain": True,
 		"query": {
@@ -333,6 +54,6 @@ def multi_match_phrase_prefix(query, fields=['writer_name']):
 		}
 	}
 
-	q = json.dumps(q)
-	return q
+	es_query = json.dumps(es_query)
+	return es_query
 
